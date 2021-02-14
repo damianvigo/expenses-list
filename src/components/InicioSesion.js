b417526dfc78e30b9ff4bed7698e9ed845937630
Damian <damiandanielvigo@gmail.com>
2021-02-14 20:34:13 -0300

    add-login

--- src/components/InicioSesion.js
+++ src/components/InicioSesion.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
+import { Header, Titulo, ContenedorHeader } from './../elements/Header';
+import Boton from './../elements/Boton';
+import { Formulario, Input, ContenedorBoton } from './../elements/ElementosDeFormulario';
+import { ReactComponent as SvgLogin } from './../assets/static/icons/login.svg';
+import styled from 'styled-components';
+
+const Svg = styled(SvgLogin)`
+  width: 100%;
+  max-height: 12.5rem; /* 200px */
+  margin-bottom: 1.25rem; /* 20px */
+`;
 
 const InicioSesion = () => {
-  return ( 
-    <h1>Inicio Sesion</h1>
-   );
-}
- 
+  return (
+    <>
+      <Helmet>
+        <title>Iniciar Sesión</title>
+      </Helmet>
+
+      <Header>
+        <ContenedorHeader>
+          <Titulo>Iniciar Sesión</Titulo>
+          <div>
+            <Boton to='/crear-cuenta'>Registrarse</Boton>
+          </div>
+        </ContenedorHeader>
+      </Header>
+
+      <Formulario>
+        <Svg />
+        <Input type='email' name='email' placeholder='Tu Correo' autoFocus />
+        <Input type='password' name='password' placeholder='Tu Contraseña' />
+        <ContenedorBoton>
+          <Boton as='button' primario type='submit'>
+          Iniciar Sesión
+          </Boton>
+        </ContenedorBoton>
+      </Formulario>
+    </>
+  );
+};
+
 export default InicioSesion;
--- src/components/RegistroUsuarios.js
+++ src/components/RegistroUsuarios.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
+import { Header, Titulo, ContenedorHeader } from './../elements/Header';
+import Boton from './../elements/Boton';
+import { Formulario, Input, ContenedorBoton } from './../elements/ElementosDeFormulario';
+import { ReactComponent as SvgLogin } from './../assets/static/icons/registro.svg';
+import styled from 'styled-components';
+
+const Svg = styled(SvgLogin)`
+  width: 100%;
+  max-height: 6.25rem; /* 100px */
+  margin-bottom: 1.25rem; /* 20px */
+`;
 
 const RegistroUsuarios = () => {
-  return ( 
-    <h1>Registro Usuarios</h1>
-   );
-}
- 
+  return (
+    <>
+      <Helmet>
+        <title>Crear Cuenta</title>
+      </Helmet>
+
+      <Header>
+        <ContenedorHeader>
+          <Titulo>Crear Cuenta</Titulo>
+          <div>
+            <Boton to='/iniciar-sesion'>Iniciar Sesion</Boton>
+          </div>
+        </ContenedorHeader>
+      </Header>
+
+      <Formulario>
+        <Svg />
+        <Input type='email' name='email' placeholder='Tu Correo' autoFocus />
+        <Input type='password' name='password' placeholder='Tu Contraseña' />
+        <Input type='password' name='password2' placeholder='Repetir la Contraseña' />
+        <ContenedorBoton>
+          <Boton as='button' primario type='submit'>
+            Crear Cuenta
+          </Boton>
+        </ContenedorBoton>
+      </Formulario>
+    </>
+  );
+};
+
 export default RegistroUsuarios;
