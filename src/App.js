b29adc706203f8b5006028fd0e6cc6a6dde54cd8
Damian <damiandanielvigo@gmail.com>
2021-02-13 23:26:09 -0300

    add-header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
+import { Header, Titulo, ContenedorHeader, ContenedorBotones } from './elements/Header'
+import Boton from './elements/Boton'
 
 const App = () => {
   return ( 
-    <h1>Hey!</h1>
+    <>
+  <Helmet>
+    <title>Agregar Gasto</title>
+  </Helmet>
+
+    <Header>
+      <ContenedorHeader>
+        <Titulo>Agregar Gasto</Titulo>
+        <ContenedorBotones>
+          <Boton to='/categorias'>Categorias</Boton>
+          <Boton to='/lista'>Lista de gastos</Boton>
+          <Boton>X</Boton>
+        </ContenedorBotones>
+      </ContenedorHeader>
+    </Header>
+    </>
    );
 }
  
--- src/components/GastosPorCategoria.js
+++ src/components/GastosPorCategoria.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Header, Titulo, ContenedorHeader } from './../elements/Header';
+import { Helmet } from 'react-helmet';
 
 const GastosPorCategoria = () => {
-  return ( 
-    <h1>Gastos por categoria</h1>
-   );
-}
- 
+  return (
+    <>
+      <Helmet>
+        <title>Gastos por Categoria</title>
+      </Helmet>
+
+      <Header>
+        <ContenedorHeader>
+          <Titulo>Gastos por categoria</Titulo>
+        </ContenedorHeader>
+      </Header>
+    </>
+  );
+};
+
 export default GastosPorCategoria;
