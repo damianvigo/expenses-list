0e0de2d752f93fe35b989f397fd7a46c71e3b836
Damian <damiandanielvigo@gmail.com>
2021-02-21 22:30:53 -0300

    layout-expenses

--- src/components/ListaDeGastos.js
+++ src/components/ListaDeGastos.js
@@ -5,11 +5,32 @@ import { Helmet } from 'react-helmet';
 import BtnRegresar from './../elements/BtnRegresar';
 import BarraTotalGastado from './BarraTotalGastado';
 import useObtenerGastos from './../hooks/useObtenerGastos';
+import {
+  Lista,
+  ElementoLista,
+  ListaDeCategorias,
+  ElementoListaCategorias,
+  Categoria,
+  Descripcion,
+  Valor,
+  Fecha,
+  ContenedorBotones,
+  BotonAccion,
+  BotonCargarMas,
+  ContenedorBotonCentral,
+  ContenedorSubtitulo,
+  Subtitulo,
+} from './../elements/ElementosDeLista';
+import IconoCategoria from './../elements/IconoCategoria';
+import convertirAMoneda from './../functions/convertirAMoneda';
+import {ReactComponent as IconoEditar} from './../assets/static/icons/editar.svg'
+import {ReactComponent as IconoBorrar} from './../assets/static/icons/borrar.svg'
+import { Link } from 'react-router-dom';
+import Boton from './../elements/Boton';
 
 const ListaDeGastos = () => {
   const [gastos] = useObtenerGastos();
-  console.log(gastos);
-
+  console.log(gastos)
   return (
     <>
       <Helmet>
@@ -20,6 +41,47 @@ const ListaDeGastos = () => {
         <BtnRegresar />
         <Titulo>Lista de Gastos</Titulo>
       </Header>
+      
+      <Lista>
+        {gastos.map((gasto) => {
+          return(
+            <ElementoLista key={gasto.id}>
+              <Categoria>
+                {/* <IconoCategoria nombre={gasto.categoria} /> */}
+                {gasto.categoria}
+              </Categoria>
+
+              <Descripcion>
+                {gasto.descripcion}
+              </Descripcion>
+
+              <Valor>
+                {convertirAMoneda(gasto.cantidad)}
+              </Valor>
+
+              <ContenedorBotones>
+                <BotonAccion as={Link} to={`/editar/${gasto.id}`}>
+                  <IconoEditar />
+                </BotonAccion>
+                <BotonAccion>
+                  <IconoBorrar />
+                </BotonAccion>
+              </ContenedorBotones>
+            </ElementoLista>
+          );
+        })}
+
+        <ContenedorBotonCentral>
+          <BotonCargarMas>Cargar más</BotonCargarMas>
+        </ContenedorBotonCentral>
+
+        {gastos.length === 0 &&
+          <ContenedorSubtitulo>
+            <Subtitulo>No hay gastos por mostrar</Subtitulo>
+            <Boton as={Link} to='/'>Agregar gasto</Boton>
+          </ContenedorSubtitulo>
+        }
+      </Lista>
 
       <BarraTotalGastado />
     </>
